eef798535c312d0d524e7ca06ceea00908a11a47
Bruno Cicero Rodrigues de Souza <brunocrodriguessouza@gmail.com>
2026-01-29 01:36:21 -0300

    configura estrutura inicial do projeto Spring Boot 4 com arquitetura hexagonal

---
+++ src/main/java/br/com/bank/voting/VotingSessionApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bank.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bank/voting/VotingSessionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bank.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSessionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
